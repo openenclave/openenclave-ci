b2962ab56986981b73ef05ce771eea90227b00a5
chrisrod-MSFT <chrisrod@microsoft.com>
2019-11-12 11:38:22 -0500

    Updated copyright header to use the new licensing header per CCC requirement.
    Signed-off-by: chrisrod-MSFT <chrisrod@microsoft.com>

--- src/jenkins/common/Openenclave.groovy
+++ src/jenkins/common/Openenclave.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/groovy
-// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Open Enclave SDK contributors.
 // Licensed under the MIT License.
 
 package jenkins.common;
